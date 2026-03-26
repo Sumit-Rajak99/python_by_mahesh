62be4ca200a101d0eb47686a045454265a5da2cf
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2026-03-26 21:47:11 +0530

    if

--- ifcondition.py
+++ ifcondition.py
@@ -1,74 +1,83 @@
-# age=int(input("enter your age: "))
+# # age=int(input("enter your age: "))
 
-# if age>=18:
-#     print("you can do vote")
+# # if age>=18:
+# #     print("you can do vote")
     
-# else:
-#     print("you can not vote")    
+# # else:
+# #     print("you can not vote")    
 
-# Q2
+# # Q2
 
-# password=int(input("enter your password: "))
-# pin=9926
-# if password==pin:
-#     print("welcome")
-# else:
-#     print("wrong password")  
+# # password=int(input("enter your password: "))
+# # pin=9926
+# # if password==pin:
+# #     print("welcome")
+# # else:
+# #     print("wrong password")  
 
-# @3 
+# # @3 
 
-# email=input("enter pyur email: ") 
-# e="sumit@gmail.com"
-# if email==e:
-#     print("correct")  
-# else:
-#     print("wrong")     
+# # email=input("enter pyur email: ") 
+# # e="sumit@gmail.com"
+# # if email==e:
+# #     print("correct")  
+# # else:
+# #     print("wrong")     
     
     
-# pass1=int(input("enter your password: "))
-# email=input("enter your email: ")    
-# p=9926
-# e="sumit@gmail.com"
+# # pass1=int(input("enter your password: "))
+# # email=input("enter your email: ")    
+# # p=9926
+# # e="sumit@gmail.com"
 
-# if pass1==9926 and e==email:
-#     print("correct")
-# else:
-#     print("wrong")    
+# # if pass1==9926 and e==email:
+# #     print("correct")
+# # else:
+# #     print("wrong")    
 
-# num=int(input("enter any number:"))
-# if num>0:
-#     print("positive")
-# else:
-#     print("negative")   
+# # num=int(input("enter any number:"))
+# # if num>0:
+# #     print("positive")
+# # else:
+# #     print("negative")   
 
-# num=int(input("enter any number"))
-# if num>=0 and num<=50:
-#     print("yes") 
-# else:
-#     print("not") 
+# # num=int(input("enter any number"))
+# # if num>=0 and num<=50:
+# #     print("yes") 
+# # else:
+# #     print("not") 
     
 
-# mark=int(input("enter your marks: "))
-# if mark>=33:
-#     print("pass")
-# else:
-#     print("fail")  
+# # mark=int(input("enter your marks: "))
+# # if mark>=33:
+# #     print("pass")
+# # else:
+# #     print("fail")  
+
+# # num=int(input("enter any number: "))
+
+# # if num>=50 and num%3==0:
+# #     print("yes")
+# # else:
+# #     print("not")
+
+
 
 # num=int(input("enter any number: "))
 
-# if num>=50 and num%3==0:
+# if num%2==0 and num%4==0:
 #     print("yes")
 # else:
-#     print("not")
+#     print("not") 
+    
+    
+# l=[1,2,3,-5,8,-9,11]
 
 
-
-num=int(input("enter any number: "))
-
-if num%2==0 and num%4==0:
-    print("yes")
-else:
-    print("not")  
+a=[1,2,3]
+b=a
+a=a+[4]
+print(b)     
 
 
            
