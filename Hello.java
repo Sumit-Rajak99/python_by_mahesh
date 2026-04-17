ace8b3dc7aba6530e38368d219167d6b13865d76
Sumit-Rajak99 <sumitrajaksumitrajak793@gmail.com>
2026-04-17 15:52:46 +0530

    new i logic learn

---
+++ Hello.java
@@ -0,0 +1,14 @@
+class Hello{
+    public static void main(String[] args){
+        int num=10;
+        if(num>8){
+            System.out.println("A");
+        }
+        else if(num==10){
+            System.out.println("B");
+        }
+        else{
+            System.out.println("c");
+        }
+    }
+}
